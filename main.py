64a9ce25c9b7e71d69c867d90bcbc4f13b0b8801
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-12 14:29:35 -0300

    Typo fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from PIL import Image
 import time
 from picamera import PiCamera
-
+pressed = False
 camera = PiCamera()
 stream = BytesIO()
 camera.start_preview()
@@ -12,13 +12,13 @@ out26 = GPIO.setup(26, GPIO.OUT)
 in19 = GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.output(26, True)
 while 1:
-  if GPIO.input(19):
+  if GPIO.input(19) and not pressed:
     print("BUTTON PRESSED")
-    camera.capture()
     camera.capture(stream, format='jpeg')
     # "Rewind" the stream to the beginning so we can read its content
     stream.seek(0)
     image = Image.open(stream)
+    pressed = not pressed
 
   time.sleep(0.5)
 
