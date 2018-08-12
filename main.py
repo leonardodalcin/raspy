33f5ea0d5c032ceeea05edfe77a1499d432b8fe1
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-12 14:28:29 -0300

    Typo fix

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import RPi.GPIO as GPIO
 from io import BytesIO
 from PIL import Image
 import time
-import picamera
+from picamera import PiCamera
 
-with picamera.PiCamera as camera:
-  stream = BytesIO()
-  camera.start_preview()
-  GPIO.setmode(GPIO.BCM)
-  out26 = GPIO.setup(26, GPIO.OUT)
-  in19 = GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-  GPIO.output(26, True)
-  while 1:
-    if GPIO.input(19):
-      print("BUTTON PRESSED")
-      camera.capture()
-      camera.capture(stream, format='jpeg')
-      # "Rewind" the stream to the beginning so we can read its content
-      stream.seek(0)
-      image = Image.open(stream)
+camera = PiCamera()
+stream = BytesIO()
+camera.start_preview()
+GPIO.setmode(GPIO.BCM)
+out26 = GPIO.setup(26, GPIO.OUT)
+in19 = GPIO.setup(19, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.output(26, True)
+while 1:
+  if GPIO.input(19):
+    print("BUTTON PRESSED")
+    camera.capture()
+    camera.capture(stream, format='jpeg')
+    # "Rewind" the stream to the beginning so we can read its content
+    stream.seek(0)
+    image = Image.open(stream)
 
-    time.sleep(0.5)
+  time.sleep(0.5)
 
 
