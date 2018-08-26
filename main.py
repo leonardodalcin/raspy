5bd380b16cefe851af32ace60a6aae6a9cca1e8e
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-26 17:08:29 -0300

    test

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 from IO import IO
 from Camera import Camera
-import time
-import cv2
-print("Vision v0.0.1")
+import code
+
+print("Beholder v0.0.1")
 print("Initializing IO")
 IO = IO()
 print("Initializing Camera")
 Camera = Camera()
-print("Taking sample photo")
-img = Camera.takePhoto()
-print(img)
-while True:
-	IO.panServo.rotate(12.5)
-	IO.tiltServo.rotate(12.5)
-	time.sleep(1)
-	IO.panServo.rotate(2)
-	IO.tiltServo.rotate(2)
-	time.sleep(1)
-	IO.panServo.rotate(7.5)
-	IO.tiltServo.rotate(7.5)
-	time.sleep(1)
 
+def repl(IO, Camera):
+    code.interact(
+        local=locals(),
+    )
 
-
+repl(IO, Camera)
