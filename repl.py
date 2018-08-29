b5a33725028a3ae56db0e4fd6de68c6d13428f84
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-29 19:33:31 -0300

    REPL

--- repl.py
+++ repl.py
@@ -1,16 +1,20 @@
 from IO import IO
 from Camera import Camera
+from IO import Input
 import code
 
 print("Beholder v0.0.1")
 print("Initializing IO")
 IO = IO()
+
 print("Initializing Camera")
 Camera = Camera()
+print("Initializing external signal input")
+sumitomoInput= Input(19, "Sumitomo", Camera.takePhoto)
 
-def repl(IO, Camera):
+def repl(IO, Camera, sumitomoInput):
     code.interact(
         local=locals(),
     )
 
-repl(IO, Camera)
+repl(IO, Camera, sumitomoInput)
