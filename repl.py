8a673f461cadfb1430f75522e807a8f4559b9dc6
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-29 21:01:11 -0300

    REPL

--- repl.py
+++ repl.py
@@ -1,19 +1,19 @@
 from IO import IO
 from Camera import Camera
 from IO import Input
-import RPi.GPIO as GPIO
-import time
+from Image import Image
 import code
 
 print("Beholder v0.0.1")
 IO = IO()
 
-def callbackFunctionTest(channel):
+def takePhoto(channel):
     print("CALLING CALLBACK FUNCTION SINCE BUTTON IS PRESSED CHANNEL " + str(channel))
+    Image(Camera.takePhoto()).save()
 
 Camera = Camera()
 
-sumitomoInput= Input(24, "Sumitomo", callbackFunctionTest)
+sumitomoInput= Input(24, "Sumitomo", takePhoto)
 
 
 def repl(IO, Camera, sumitomoInput):
