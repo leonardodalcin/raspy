d2c45a990fc596307d08d108d27ec72cdfc934dd
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-30 11:22:10 -0300

    Image test

--- Cameratest.py
+++ Cameratest.py
@@ -1,5 +1,11 @@
-from time import sleep
 from picamera import PiCamera
+from picamera.array import PiRGBArray
+import cv2
+
+
 
 camera = PiCamera()
-camera.capture('foo.jpg')
+rawCapture = PiRGBArray(camera)
+camera.capture(rawCapture, format="bgr")
+img = rawCapture.array()
+cv2.imwrite("test.png", img)
