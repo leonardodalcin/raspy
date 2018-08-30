406758b3c26ea8d3117641f32efbaeee23d73471
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-30 11:34:21 -0300

    Image test

--- Cameratest.py
+++ Cameratest.py
@@ -9,4 +9,12 @@ rawCapture = PiRGBArray(camera)
 camera.rotation = 180
 camera.capture(rawCapture, format="bgr")
 img = rawCapture.array
+# 180 degrees
+(h, w) = img.shape[:2
+                    ]
+# calculate the center of the image
+center = (w / 2, h / 2)
+M = cv2.getRotationMatrix2D(center, 180, 1)
+
+rotated180 = cv2.warpAffine(img, M, (w, h))
 cv2.imwrite("test.png", img)
