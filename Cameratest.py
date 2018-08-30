dc10bc72763a7b36f38b208c36d0b6f6fecc0ffa
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-30 11:35:14 -0300

    Image test

--- Cameratest.py
+++ Cameratest.py
@@ -6,7 +6,6 @@ import cv2
 
 camera = PiCamera()
 rawCapture = PiRGBArray(camera)
-camera.rotation = 180
 camera.capture(rawCapture, format="bgr")
 img = rawCapture.array
 # 180 degrees
@@ -17,4 +16,4 @@ center = (w / 2, h / 2)
 M = cv2.getRotationMatrix2D(center, 180, 1)
 
 rotated180 = cv2.warpAffine(img, M, (w, h))
-cv2.imwrite("test.png", img)
+cv2.imwrite("test.png", rotated180)
