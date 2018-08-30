da112e3685dfceaa39cbae3e129be87ce4247d2d
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-08-29 21:07:07 -0300

    REPL

--- repl.py
+++ repl.py
@@ -6,12 +6,15 @@ import code
 
 print("Beholder v0.0.1")
 IO = IO()
+Camera = Camera()
+
+Image(Camera.takePhoto()).save()
+
 
 def takePhoto(channel):
     print("CALLING CALLBACK FUNCTION SINCE BUTTON IS PRESSED CHANNEL " + str(channel))
     Image(Camera.takePhoto()).save()
 
-Camera = Camera()
 
 sumitomoInput= Input(24, "Sumitomo", takePhoto)
 
