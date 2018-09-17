9f8fee738d69e86d9e7878c627d137e03d0f147c
Leonardo Dalcin <leonardo.dalcin@qmoni.com>
2018-09-16 21:54:41 -0300

    Full Resolution

--- Camera.py
+++ Camera.py
@@ -35,7 +35,9 @@ class Camera:
 			raise Exception("This class is a singleton!")
 		else:
 			print("Setting PiCamera wrapper")
-			self.piCamera = PiCamera(sensor_mode=2)
+			self.piCamera = PiCamera(sensor_mode=2,
+									 resolution='3280x2464',
+									 framerate=15)
 			Camera.__instance = self
 
 
